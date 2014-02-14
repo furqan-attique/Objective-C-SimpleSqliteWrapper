690435d9e26d5f55113710ced81a2a30058450ee
Furqan <furqan@Furqans-MacBook-Pro.local>
2014-02-14 19:20:43 +0500

    Initial Commit

---
+++ FASqliteWrapper-ObjC/FAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FAAppDelegate.h
+//  FASqliteWrapper-ObjC
+//
+//  Created by Furqan on 2/14/14.
+//  Copyright (c) 2014 InnovativeApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FASqliteWrapper-ObjC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FASqliteWrapper-ObjC
+//
+//  Created by Furqan on 2/14/14.
+//  Copyright (c) 2014 InnovativeApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FAAppDelegate class]));
+    }
+}
